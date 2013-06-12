1566cd2375f67ba50fc0f6d11c7cf960c3f23cf9
Mikołaj Hnatiuk <mikolaj@HAL9000.(none)>
2013-06-12 11:58:27 +0200

    Dodalem import local_settings zeby nie trzymac hasla na githubie :)

--- edudata/edudata/settings.py
+++ edudata/edudata/settings.py
@@ -1,6 +1,9 @@
 # Django settings for edudata project.
 import os
 
+from local_settings import *
+
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -10,16 +13,6 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'django',                      # Or path to database file if using sqlite3.
-        'USER': 'django',                      # Not used with sqlite3.
-        'PASSWORD': 'UD8QaCXiHjGDZrdOPRvO',                  # Not used with sqlite3.
-        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
