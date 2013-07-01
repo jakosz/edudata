148001f38ad6383dcba8dfed190241dd82ec93d7
Jacek Szejda <jakosz@gmail.com>
2013-07-01 16:52:58 -0400

    poprawiłem kilka błędów

--- edudata/research_db/edudata_helpers.py
+++ edudata/research_db/edudata_helpers.py
@@ -14,9 +14,9 @@ from django.core.files import File
 err_msg = u'To nie jest poprawny plik csv; {0}.'
 non_txt = u'podany plik powinien być typu tekstowego'
 non_utf = u'podany plik powinien być zakodowany wg standardu utf-8'
-non_tab = u'we wszystkich wierszach liczba kolumn powinna być taka sama, tymczasem liczba kolumn w wierszu nr 1 i wierszu nr {0} jest różna.'
-non_sep = u'separatorem kolumn powinien być średnik ( ; ). Błąd został znaleziony w wierszu nr {0}.'
-non_quo = u'komórki zawierające tekst powinny być otoczone cudzysłowem ( " ). Błąd został znaleziony w wierszu nr {0}.'
+non_tab = u'we wszystkich wierszach liczba kolumn powinna być taka sama, tymczasem liczba kolumn w wierszu nr 1 i wierszu nr {0} jest różna'
+non_sep = u'separatorem kolumn powinien być średnik ( ; ). Błąd został znaleziony w wierszu nr {0}'
+non_quo = u'komórki zawierające tekst powinny być otoczone cudzysłowem ( " ). Błąd został znaleziony w wierszu nr {0}'
 hSpaces = u'nazwy zmiennych nie powinny zawierać znaku spacji.'
 
 # Checks if a file is of "text/.*$" mimetype:
@@ -27,8 +27,10 @@ def is_txt(ff):
 
 # Checks if a text file encoding is utf-8:
 def is_utf8(ff):
-    if not chardet.detect(File(ff).read(1024))['encoding'] == 'utf-8':
-        if not chardet.detect(File(ff).read(1024))['confidence'] == 0 or not chardet.detect(File(ff).read(1024))['encoding'] == None:
+    ch = chardet.detect(File(ff).read(65536))
+    if not ch['encoding'] == 'utf-8' and not ch['encoding'] == 'ascii':
+        if not ch['confidence'] == 0 and not ch['encoding'] == None:
+            print ch
             raise ValidationError, _(err_msg.format(non_utf))
         else:
             warn('is_utf8: chardet.detect couldn\'t determine file encoding')
@@ -36,15 +38,19 @@ def is_utf8(ff):
 
 # Checks if all non-numeric cells are quoted with the desired character:
 def is_quote_ok(ff, sep = ';', quote = '"', check_header = False):
-    f = File(ff).read()
-    for i, row in enumerate(f[check_header:]):
-        cRow = row.split(sep)
+    # read csv with quotechar set as the BEL UTF code (0x07), so we can perform
+    # check using the intended character (quote).
+    f = gen2list(csv.reader(ff, delimiter = sep, quotechar = '\x07'))
+    for i, cRow in enumerate(f[not check_header:]):
+        print cRow
         for cell in cRow:
             try:
+                # try to convert whatever you've found to a float:
                 float(cell)
             except ValueError:
-                if not cell.startswith(quote) and not cell.endswith(quote):
-                    raise ValidationError, _(err_msg.format(non_quo.format(str(i))))
+                # if it's impossible, then cell contents should be quoted:
+                if not cell.startswith(quote) and not cell.endswith(quote) and not cell == '':
+                    raise ValidationError, _(err_msg.format(non_quo.format(str(i + (not check_header)))))
     print 'is_quote_ok: True'
 
 # Checks if a number of columns is the same for all rows:
@@ -52,13 +58,10 @@ def is_table(ff, sep = ';', quote = '"'):
     f = csv.reader(ff, delimiter = sep, quotechar = quote)
     rowCounter = 0
     firstRow = f.next()
-    while True:
-        try:
-            rowCounter += 1
-            if len(firstRow) != len(f.next()):
-                raise ValidationError, _(err_msg.format(non_tab.format(str(rowCounter+1))))
-        except StopIteration:
-            break
+    for row in f:
+        rowCounter += 1
+        if len(firstRow) != len(row):
+            raise ValidationError, _(err_msg.format(non_tab.format(str(rowCounter+1))))
     print 'is_table: True'
 
 # Checks if variable names in the header don't contain space characters:
@@ -79,6 +82,16 @@ def is_sep_ok(ff, sep = ';', quote = '"'):
     if len(f.next()) == 1:
         raise ValidationError, _(err_msg.format(non_sep))
     print 'is_sep_ok: True'
+
+def gen2list(Generator):
+    List = []
+    for item in Generator:
+        List.append(item)
+    return List
+
+def list2gen(List):
+    for item in List:
+        yield item
 
 # Performs all of the above tests; used as a validation function in edudata/models
 def is_csv(FieldFile_file, sep = ';', quote = '"'):
