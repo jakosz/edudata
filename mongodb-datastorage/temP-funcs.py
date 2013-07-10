67ae9911d7f3ecb17b20943a89a1b93fc3fa7a4d
John Anderson <sontek@gmail.com>
2013-07-10 11:21:08 +0200

    .gitignore

---
+++ mongodb-datastorage/temP-funcs.py
@@ -0,0 +1,75 @@
+import csv
+import json
+from pandas import DataFrame as psDataFrame
+from pymongo import MongoClient
+
+def mongodb_prepare(cb_reader, pandas_df, df_name):
+    data = dict()
+    data['df_name'] = df_name
+    data['columns'] = []
+    for nr,row in enumerate(cb_reader):
+        column = dict()
+        
+        #data[df_name] = 
+        #data[var_name] = dict()
+        #data[var_name]['df_name']=df_name        
+        column['name'],column['desc_short'], column['desc_long'],\
+            column['keywords'], column['type'],\
+            column['scale'], column['value_len'],\
+            column['precision'], column['min_value'],\
+            column['max_value'], column['labels'],\
+            column['condition'] = row
+        try:
+            var_data = pandas_df[ column['name'] ].to_dict()
+            
+            column['data'] = dict((str(key), value) for (key, value) in var_data.items()) 
+        except KeyError:
+            e= "Failed to locate '{}' var in {} df columns ".format(column['name'], pandas_df.columns)
+            raise KeyError, e
+        data['columns'].append(column)
+        
+    return data
+
+def process_data(db):
+    codebook = open("codebook.csv", "r")
+    df = open ("df.csv","r")
+    cb_reader = csv.reader(codebook, delimiter = ';', quotechar = '"')
+    cb_reader.next() ## skip the header of the codebook!
+    data_reader =  csv.reader(df,delimiter=";",quotechar='"')
+    data_header = data_reader.next() ## get header
+    df_rows = [row for row in data_reader]                 
+    pandas_df = psDataFrame(df_rows,columns=data_header)
+    insert_query  =mongodb_prepare(cb_reader,pandas_df, 'pierszytest')
+    dataframes = db.dataframes
+    dataframes.insert(insert_query)
+
+
+
+def get_dataframe(df_name):
+    dataframes = db.dataframes
+    cursor = db.dataframes.find({'df_name':'pierszytest'})
+    df = cursor.next()
+    dfdict = dict()
+    rownames = []
+    # TU MUSI BYĆ SORTOWANIE po kluczu i to kluczu INTEGER!!
+    for nr,column in enumerate(df['columns']): # type(df['columns']) == type([])
+        print column['name'], " - ",nr
+        if nr == 0:
+            rownames = column['data'].keys()
+        rows = dict((int(key), value) for (key, value) in column['data'].items()) 
+        dfdict[column['name']] = [value for (key,value) in rows.iteritems()]
+
+    pandas_df = psDataFrame(dfdict, index = rownames )
+    pandas_df.to_csv("df_out.csv", sep=";", quoting=csv.QUOTE_NONNUMERIC,encoding="utf-8",float_format=".")
+        #data.append(columns['data'])
+        
+    
+client = MongoClient()
+db = client.dataframes
+process_data(db)
+
+
+
+#def mongodb_insert(bson_data):
+
+
