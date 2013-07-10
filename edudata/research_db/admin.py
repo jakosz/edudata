342b9dcbd0feb4a8d36b87d14b0725d4e7ff7a12
John Anderson <sontek@gmail.com>
2013-07-10 18:33:20 +0200

    Naprawilem niewyswietlajace sie edytowanie w Markdown w widoku admina dla researchproject/add

--- edudata/research_db/admin.py
+++ edudata/research_db/admin.py
@@ -10,21 +10,27 @@ class NestedProductInline(NestedTabularInline):
     model = research_db.models.Product
     extra = 3
 class DataframeInline(NestedStackedInline):
+    formfield_overrides = {
+            models.TextField: {
+                'widget':AdminPagedownWidget},
+            }
     model = research_db.models.Dataframe
     inlines = [NestedProductInline]
     extra =1
+    exclude = ('sampling_description_html',)
 
 class DataframeAdmin(admin.ModelAdmin):
     formfield_overrides = {
             models.TextField: {'widget':AdminPagedownWidget},
             }
-
+    exclude = ('sampling_description_html',)
 
 class ResearchProjectAdmin(NestedModelAdmin):
     inlines = [DataframeInline]
     formfield_overrides = {
             models.TextField: {'widget':AdminPagedownWidget},
             }
+    exclude = ('sampling_description_html',)
 
 admin.site.register(research_db.models.ResearchProject, ResearchProjectAdmin)
 admin.site.register(research_db.models.Dataframe, DataframeAdmin)
