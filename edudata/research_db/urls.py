f60170852e7e3c0bf92d0f532e53db0067e43c23
John Anderson <sontek@gmail.com>
2013-07-10 18:34:07 +0200

    Dodalem url do dataframe/detail oraz dataframe/basicinfo

--- edudata/research_db/urls.py
+++ edudata/research_db/urls.py
@@ -3,6 +3,7 @@
 from django.conf.urls import patterns, include, url
 #from django.contrib import admin
 from django.views.generic import DetailView, ListView
+from research_db.views import *
 from research_db.models import ResearchProject, Dataframe
 
 
@@ -14,17 +15,11 @@ do widokow:
 	* index - lista wszystkich projektu badawczego
 	* detail - szczegoly na temat projektu badawczego
 """
-
 urlpatterns = patterns('',
 	url(r'^project/$',
-		ListView.as_view(
-			queryset=ResearchProject.objects.order_by('-pub_date')[:5],
-			context_object_name='latest_research_project_list',
-			template_name='research_db/index.html')),
-	url(r'^project/(?P<pk>\d+)/$',
-		DetailView.as_view(
-			model=ResearchProject,
-			template_name='research_db/detail.html')),
+		ResearchProjectIndex.as_view(), name= "list"),
+	url(r'^project/(?P<pk>\d+)/$', ResearchProjectDetail.as_view(), name = "detail"),
+        url(r'^dataframe_basicinfo/(?P<pk>\d+)/$', dataframe_basicinfo, name="dataframe_basicinfo")
 )
 
 
