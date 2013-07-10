d4ccb4cb7c6bae6b6c7c9b88f5bbef79bd310aff
John Anderson <sontek@gmail.com>
2013-07-10 18:31:09 +0200

    Dodalem kilka funkcji do wystwietlania uzytkownikowi (nie adminowi) szczegolow dotyczacych: Resaerch Project, Dataframe. Dodalem tez wystwietlanie podstawowych statystyk nt. ramki danych

--- edudata/research_db/views.py
+++ edudata/research_db/views.py
@@ -4,24 +4,51 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views import generic
+from research_db.decorators import *
+from research_db.models import ResearchProject, Dataframe
 
-from research_db.models import ResearchProject, DataFrame
-
-class IndexView(generic.ListView):
-    template_name = 'research_db/index.html'
-    context_object_name = 'latest_researchproject_list'
-
+class ResearchProjectIndex(generic.ListView):
+    print "RP INDEX"
+    template_name = 'research_db/researchproject/index.html'
+    context_object_name = 'researchproject_list'
     def get_queryset(self):
         """Return the last five published research projects."""
-        return ResearchProject.objects.order_by('-pub_date')[:5]
+        print ResearchProject.objects.all()
+        return ResearchProject.objects.all()
 
 
-class DetailView(generic.DetailView):
+class ResearchProjectDetail(generic.DetailView):
     model = ResearchProject
-    template_name = 'research_db/detail.html'
+    template_name = 'research_db/researchproject/detail.html'
 
 
-def vote(request, poll_id):
-    pass
 
+@html_row
+def render_codebook_row(column):
+    row_html = ""
+    print "!!!!!",column
+    def html_cell(content):
+        return "<td>"+content+"</td>"
+    for key,val in column.iteritems():
+        row_html += html_cell(val)
+    return row_html
+
+@html_bstrap_table
+def render_codebook_info(codebook_info):
+    table_html = ""
+    for key,value in codebook_info.iteritems():
+        table_html += render_codebook_row(value)
+    return table_html
+
+
+def dataframe_basicinfo(request, pk):
+    dataframe = get_object_or_404(Dataframe, pk=pk)
+    data_and_info = dataframe.get_data()
+    psDf = data_and_info['df']
+    codebook_info = data_and_info['info']
+
+    summary_html = psDf.describe().to_html(classes=('table','table-stripped','table-bordered'))
+    return render(request, 'research_db/dataframe/basicinfo.html',
+            {'dataframe': dataframe, 'codebook_info': render_codebook_info(codebook_info),
+                'summary_html':summary_html})
 
