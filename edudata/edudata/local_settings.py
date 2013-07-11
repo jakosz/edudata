7feaa02792dcdcd18ddc13dee6995288868254e5
root <root@ks39015.kimsufi.com>
2013-07-11 11:38:57 +0200

    Commiting uncommited changes

---
+++ edudata/edudata/local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+	'USER' : 'django',
+	'PASSWORD' : 'yHmGosB^',
+	'HOST':'127.0.0.1',
+	'NAME':'django',
+        },
+    }
+
