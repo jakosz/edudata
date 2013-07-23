262e7aea90a03016d89a150ee1e29116523b4568
mhnatiuk <mikolaj.hnatiuk@gmail.com>
2013-07-23 12:08:32 +0200

    Ukrywanie pol trzymajacych html

--- edudata/research_db/admin.py
+++ edudata/research_db/admin.py
@@ -18,13 +18,13 @@ class DataframeInline(NestedStackedInline):
     model = research_db.models.Dataframe
     inlines = [NestedProductInline]
     extra =1
-    exclude = ('sampling_description_html',)
+    exclude = ('sampling_description_html','response_rate_html',)
 
 class DataframeAdmin(admin.ModelAdmin):
     formfield_overrides = {
             models.TextField: {'widget':AdminPagedownWidget},
             }
-    exclude = ('sampling_description_html',)
+    exclude = ('sampling_description_html','citation_html',)
 
 class ResearchProjectAdmin(NestedModelAdmin):
     
@@ -35,7 +35,9 @@ class ResearchProjectAdmin(NestedModelAdmin):
             #models.TextField: {'widget': Textarea(attrs={'rows':30,'cols':120})},
             
             }
-    exclude = ('project_description_html',)
+    exclude = ('project_description_html','citation_html',)
+
+
 
 admin.site.register(research_db.models.ResearchProject, ResearchProjectAdmin)
 admin.site.register(research_db.models.Dataframe, DataframeAdmin)
